aa11913a8d880316bfa547885537c968b67f08b9
Felipe Costa <felipecosta8449@hotmail.com>
2020-06-20 12:43:03 -0300

    Commit inicial.
    Código básico para a implementação e visualização de um passeio
    aleatório.

---
+++ MyScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+     transform.localPosition = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 randomWalk = new Vector3(Random.Range(-1f, 1f),Random.Range(-1f,1f),Random.Range(-1f, 1f));
+        transform.Translate(randomWalk.x * Time.deltaTime,randomWalk.y * Time.deltaTime,randomWalk.z * Time.deltaTime);
+    }
+}
